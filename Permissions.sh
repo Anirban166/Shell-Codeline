186cc52573fdae26930352cc52dbbb588c889659
Anirban Chetia <bloodraven166@gmail.com>
2019-08-08 00:09:36 +0530

    Update Permissions.sh

--- Permissions.sh
+++ Permissions.sh
@@ -1,9 +1,9 @@
 echo Enter Filename
 read $fname
 if [ -r $fname -o -w $fname ]
-then echo File has read or write privileges
+then echo File has Read or write privileges
 elif [ -r $fname -a -w $fname ]
-then echo File has both read and write privileges
+then echo File has both Read and write privileges
 else echo File has no permissions granted
 fi
 
