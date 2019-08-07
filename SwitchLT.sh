c0c6c48309ae74c15beaed3dc36079f95bcc812e
Anirban Chetia <bloodraven166@gmail.com>
2019-08-07 23:59:49 +0530

    Switch case. (Terminal-based)

---
+++ SwitchLT.sh
@@ -0,0 +1,14 @@
+echo Note : Terminal commands used / Use Linux.
+
+read ch
+case $ch in
+1) ls -l ;;
+2) date ;;
+3) pwd ;;
+4) bc ;;
+5) who ;;
+6) whoami ;;
+7) ps ;;
+*) echo Invalid choice
+exit ;;
+esac
