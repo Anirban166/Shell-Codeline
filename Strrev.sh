c98b3aa0d78a076fd1ee0b10e48aaee0b908ec5c
Anirban Chetia <bloodraven166@gmail.com>
2019-08-10 19:17:58 +0530

    String reverse.

---
+++ Strrev.sh
@@ -0,0 +1,17 @@
+tput clear
+echo Enter a string :
+cat > str
+len=`cat str | wc -c`
+len=`expr $len - 1`
+echo Length of the reversed string is $len
+echo Reversed string is :
+row=5
+col=2
+while [ $l -ge 1 ]
+do
+ rev=`cat str | cut -c $len`
+ tput cup $row $col
+ echo $rev
+ len=`expr $len - 1`
+ col=`expr $col + 1`
+done
