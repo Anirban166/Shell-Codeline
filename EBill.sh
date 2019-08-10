c737c497d28d29b8920c884d56c81fdc6d797acd
Anirban Chetia <bloodraven166@gmail.com>
2019-08-10 10:30:18 +0530

    Electricity Bill.

--- EBill.sh
+++ EBill.sh
@@ -1,7 +1,7 @@
-echo Rates for Electricity Bill :
-echo 5/unit + 50 for units<101.
-echo 8/unit + 50 for units>100 units<201
-echo 8/unit + 80 for units>200
+echo Rates For Electricity Bill :
+echo 5/unit + 50 For units<101.
+echo 8/unit + 50 For units>100 units<201
+echo 8/unit + 80 For units>200
 echo Enter units consumed
 read u
 if [ $u -lt 101 ]
@@ -10,4 +10,4 @@ elif [ $u -gt 100 -a -lt 201 ]
 then bill=`expr $u \* 8 + 50`
 else bill=`expr $u \* 8 + 80`
 fi
-echo Your bill is $bill
+echo Your bill is INR $bill.
